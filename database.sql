5696f7ff0fca57a41e02fab7f4e518180b1b5373
Chinmaya Pulikallu <chinmayapulikallu@gmail.com>
2020-05-26 16:44:30 -0500

    added database

--- database.sql
+++ database.sql
@@ -8,3 +8,29 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "event" (
+"id" SERIAL PRIMARY KEY,
+"event_name" VARCHAR (100) NOT NULL,
+"event_location" VARCHAR(250) NOT NULL,
+"event_address_line1" VARCHAR(500),
+"event_address_line2" VARCHAR(500),
+"max_participants" INTEGER
+);
+
+CREATE TABLE "dancer" (
+"id" SERIAL PRIMARY KEY,
+"first_name" VARCHAR(80) NOT NULL,
+"last_name" VARCHAR(80) NOT NULL,
+"contact_number" INTEGER,
+"email_id" VARCHAR(200)
+);
+
+CREATE TABLE "dance_styles" (
+"id" SERIAL PRIMARY KEY,
+"dance_form" VARCHAR(255)
+);
+
+CREATE TABLE "dancer_events" (
+"id" SERIAL PRIMARY KEY
+);
