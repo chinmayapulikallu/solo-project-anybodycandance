deaec2bbcce281cb1cfaac0b3a5d619541ca957d
Chinmaya Pulikallu <chinmayapulikallu@gmail.com>
2020-06-02 16:34:22 -0500

    added card to my events

--- src/components/MyEvents/MyEvents.js
+++ src/components/MyEvents/MyEvents.js
@@ -1,5 +1,52 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+//material - ui
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+// import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import Grid from '@material-ui/core/Grid';
+
+
+
+const useStyles = (theme) => ({
+    root: {
+        // maxWidth: 345,
+        borderColor: 'red',
+        // backgroundColor: '#80deea',
+        flexGrow: 1,
+        margin: 30
+    },
+    media: {
+        height: 250,
+        width: 250,
+        padding: 25
+        // height: 0,
+        // paddingTop: '56.25%', // 16:9
+    },
+    avatar: {
+        backgroundColor: red[500],
+        width: 60,
+    },
+    paper: {
+        height: 500,
+        width: 300,
+    },
+    control: {
+        padding: theme.spacing(2),
+    },
+    cardPadding: {
+        padding: 30
+    }
+
+});
+
 
 class MyEvents extends Component {
 
@@ -15,9 +62,67 @@ class MyEvents extends Component {
     }
 
     render() {
+        const { classes } = this.props;  
         return (
             <div>
                 <h2>My Events</h2>
+                <div className="new-event-image">
+                    <Grid container className={classes.root} spacing={2}>
+                        <Grid item xs={12}>
+                            <Grid container justify="flex-start" spacing={9}>
+                                {this.props.events.map(event =>
+                                    <span key={event.id}>
+                                        <Grid key={event.id} item className={classes.cardPadding}>
+                                            <Card variant="outlined" className={classes.paper}>
+                                                <CardHeader
+                                                    avatar={
+                                                        <Avatar aria-label="recipe" className={classes.avatar}>
+                                                          
+                                                        </Avatar>
+                                                    }
+                                                    action={
+                                                        <IconButton aria-label="settings">
+                                                        </IconButton>
+                                                    }
+                                                    title={event.event_name}
+                                                />
+                                                <CardMedia
+                                                    component="img"
+                                                    className={classes.media}
+                                                    image={event.event_image}
+                                                />
+                                                <CardContent>
+                                                    {/* <Typography variant="body2" color="textSecondary" component="p">
+                                                        {event.event_description}
+                                                    </Typography> */}
+                                                    {/* {event.event_dancer_count > event.current_dancer_count &&
+                                                        <Button variant="contained" color="primary"
+                                                            onClick={() => { this.joinEvent(event.id) }}>Join</Button>} */}
+
+                                                </CardContent>
+                                                <CardActions>
+
+                                                </CardActions>
+                                            </Card>
+                                        </Grid>
+
+                                    </span>
+                                )}
+
+                            </Grid>
+                        </Grid>
+
+                    </Grid>
+
+                </div>  
+
+
+
+
+
+
+
+
                 <table>
                     <thead>
                         <tr>
@@ -46,4 +151,4 @@ const putReduxStateOnProps = (reduxState) => ({
     user: reduxState.user
 })
 
-export default (connect(putReduxStateOnProps)(MyEvents));
+export default (withStyles(useStyles))(connect(putReduxStateOnProps)(MyEvents));
